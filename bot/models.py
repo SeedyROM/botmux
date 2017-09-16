10e7a12cd1bc43a11d36d4bda2f5efdcf94b0a63
Zack Kollar <rallokkcaz@gmail.com>
2017-09-16 04:06:00 -0700

    Working on working.

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from core.models import UUIDModel
+
+
+class Bot(UUIDModel):
+    """Bot data encapsulation.
+    """
+    # TODO: 
+    # [ ] Allow for bot protocol definition.
+    # [ ] Corpus relation.
+    name = models.CharField(max_length=128)
--- core/models.py
+++ core/models.py
@@ -1,16 +1,16 @@
-"""Core module's models and classes.
+"""Core module's models.
 """
 
-
 from django.db import models
-from django_smalluuid.models import SmallUUIDField, uuid_default
+from django_smalluuid.models import SmallUUIDField, uuid_typed_default
+
 
 class UUIDModel(models.Model):
     """Abstarct model to create unsequential indentifiers.
     """
 
     # pylint: disable=C0103
-    id = SmallUUIDField(default=uuid_default())
+    id = SmallUUIDField(default=uuid_typed_default(type=42))
 
     class Meta:
         abstract = True
