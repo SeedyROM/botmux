906de7e663dc046b9be2f9f28f1d0e2e800809a7
Zack Kollar <rallokkcaz@gmail.com>
2017-09-16 03:40:45 -0700

    Adde some more specification.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+"""Core module's models and classes.
+"""
 
-# Create your models here.
+
+from django.db import models
+from django_smalluuid.models import SmallUUIDField, uuid_default
+
+class UUIDModel(models.Model):
+    """Abstarct model to create unsequential indentifiers.
+    """
+
+    # pylint: disable=C0103
+    id = SmallUUIDField(default=uuid_default())
+
+    class Meta:
+        abstract = True
